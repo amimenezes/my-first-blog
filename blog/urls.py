125ce2dfa38a64f2f3cb2b0977c76dc48aab3c2f
mml88 <mi.menezes.luiz@gmail.com>
2016-11-04 20:16:58 -0200

    Correções!!!

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-=======
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.post_list),
->>>>>>> bbb4568a9bcf8b05028422ba8c25b0d22b25c900
 ]
